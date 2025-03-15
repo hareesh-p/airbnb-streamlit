144e6b9c6dbed0edf6fc1ba1e63044110b15fd62
Hareesh <hareesh2007@gmail.com>
2025-03-15 20:20:44 +0530

    Fixed Sentiment for Streamlit Cloud

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -9,6 +9,7 @@ from wordcloud import WordCloud
 
 # Download necessary NLTK resources
 nltk.download("punkt")
+nltk.download('punkt_tab')
 nltk.download("stopwords")
 nltk.download("vader_lexicon")
 
